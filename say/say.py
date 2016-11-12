bfdcdad165dfeb92fc3cf950965f47c0c271a1d6
Jan200101 <sentrycraft123@gmail.com>
2016-11-12 14:13:20 +0100

    Updated say

--- say/say.py
+++ say/say.py
@@ -1,5 +1,7 @@
 from discord.ext import commands
 from cogs.utils import checks
+from cogs.utils.chat_formatting import box
+from cogs.utils.chat_formatting import pagify
 
 class say:
     """Makes the bot say things for you. Great with the Schedule cog"""
@@ -26,7 +28,14 @@ class say:
         if ctx.message.author == "238685395838042113":
             await self.bot.say("Stop using my bot for making it say simple stuff")
         else:
-            await self.bot.say(text + " (message by " + ctx.message.author.mention + ")")
+            text += "(message by"
+            text += ctx.message.author
+            text += ")"
+            for page in pagify(text, ["\n"], shorten_by=13, page_length=2000):
+                await self.bot.say(page)
 
 def setup(bot):
     bot.add_cog(say(bot))
+
+        for page in pagify(data, ["\n"], shorten_by=13, page_length=2000):
+            await self.bot.say(box(page, 'Prolog'))
