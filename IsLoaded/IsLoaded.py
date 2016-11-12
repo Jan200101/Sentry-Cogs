290c5f5f1b32098cc5e43cab7d48207df61c8d4b
Jan200101 <sentrycraft123@gmail.com>
2016-11-12 01:58:50 +0100

    Updated IsLoaded to see for the Cog filename instead of the classname. Keeping the classname command for debug

--- IsLoaded/IsLoaded.py
+++ IsLoaded/IsLoaded.py
@@ -7,15 +7,25 @@ class IsLoaded:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="IsLoaded", no_pm=True)
+    @commands.command(name="CogLoaded")
     @checks.is_owner()
     async def cog_loaded(self, cog: str):
-        """Checks if a Cog is loaded or not (unmaintained)"""
+        """Checks if a Cog is loaded or not"""
+
+        if cog in bot.cogs
+            await self.bot.say("""```Py\nCog "{}" is not loaded```""".format(cog))
+        else:
+            await self.bot.say("""```Py\nCog "{}" is loaded```""".format(cog))
+
+    @commands.command(name="ClassLoaded")
+    @checks.is_owner()
+    async def class_loaded(self, cog: str):
+        """Checks if a Cogs class is loaded or not (unmaintained)"""
 
         if self.bot.get_cog(cog)== None:
-            await self.bot.say("""```Py\n Module with the class "{}" is not loaded```""".format(cog))
+            await self.bot.say("""```Py\nClass "{}" is not loaded```""".format(cog))
         else:
-            await self.bot.say("""```Py\n Module with the class "{}" is loaded```""".format(cog))
+            await self.bot.say("""```Py\nClass "{}" is loaded```""".format(cog))
 
 def setup(bot):
     n = IsLoaded(bot)
