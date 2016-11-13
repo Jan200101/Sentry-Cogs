5075e340bf6c2ed9329323ef5cac49057a1655a0
Jan200101 <sentrycraft123@gmail.com>
2016-11-13 10:26:45 +0100

    anti SentryBot Nep

--- Nep/Nep.py
+++ Nep/Nep.py
@@ -6,12 +6,17 @@ class Nep:
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.command(no_pm=True)
     async def nep(self):
         """Displays a random Nep."""
-        link = ["http://i.imgur.com/13hoMVJ.jpg", "http://i.imgur.com/kIzXdwN.jpg", "http://i.imgur.com/DICh64t.jpg", "http://i.imgur.com/nMp3NMp.png", "http://i.imgur.com/MMf1YfR.png", "http://i.imgur.com/CGABJEs.jpg", "http://i.imgur.com/GRz1oCo.jpg"]
-        nep = choice(link)
-        await self.bot.say(nep)
+
+        if settings.owner == "137268543874924544":
+            raise Exception("Please stop putting my bot through this")
+        else:
+            link = ["http://i.imgur.com/13hoMVJ.jpg", "http://i.imgur.com/kIzXdwN.jpg", "http://i.imgur.com/DICh64t.jpg", "http://i.imgur.com/nMp3NMp.png", "http://i.imgur.com/MMf1YfR.png", "http://i.imgur.com/CGABJEs.jpg", "http://i.imgur.com/GRz1oCo.jpg"]
+            nep = choice(link)
+            await self.bot.say(nep)
 
 def setup(bot):
     n = Nep(bot)
