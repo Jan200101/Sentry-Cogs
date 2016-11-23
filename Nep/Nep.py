c23994853b82a926acd06bad74b14aa57a09d2e9
Sentry <sentrycraft123@gmail.com>
2016-11-23 18:05:00 +0100

    random text [Nep]

--- Nep/Nep.py
+++ Nep/Nep.py
@@ -16,11 +16,14 @@ class Nep:
         nep = ["http://i.imgur.com/13hoMVJ.jpg", "http://i.imgur.com/kIzXdwN.jpg", "http://i.imgur.com/DICh64t.jpg", "http://i.imgur.com/nMp3NMp.png", "http://i.imgur.com/MMf1YfR.png", "http://i.imgur.com/CGABJEs.jpg", "http://i.imgur.com/GRz1oCo.jpg"]
         nep = choice(nep)
 
+        nepsay =["Nep!!11","Neeeeeeepppppp","Neeeeeeeeeeeeeeeeeeeeeeepppppppppp","Nep Nep :P","*intense nepping*","I ran out of Nep so here is some more"]
+        nepsay = choice(nepsay)
+
         colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
         colour = int(colour, 16)
 
         data = discord.Embed(description="", colour=discord.Colour(value=colour))
-        data.add_field(name="Nep", value=u"\u2063")
+        data.add_field(name=nepsay, value=u"\u2063")
         data.set_image(url=nep)
 
         try:
