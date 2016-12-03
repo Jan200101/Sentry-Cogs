c6fdf680013b4abde5f7fe7aa6eaadd7a21baec8
Sentry <sentrycraft123@gmail.com>
2016-12-03 17:05:15 +0100

    [say] Added embedurl command       suggested by Anismash#2803

--- say/say.py
+++ say/say.py
@@ -21,8 +21,9 @@ class say:
         try:
             await self.bot.delete_message(ctx.message)
         except:
-            raise Exception("I do not have the permissions needed")
-            
+            await self.bot.say("I do not have the `Manage Messages` permissions")
+            return
+
         for text in pagify(text, ["\n"]):
             await self.bot.say(escape_mass_mentions(text))
 
@@ -30,9 +31,8 @@ class say:
     async def say(self, ctx, *, text):
         """Says Something as the bot """
 
-        auth = " (message by "
-        auth += ctx.message.author.mention
-        auth += ")"
+        auth = " (message by {})".format(ctx.message.author.mention)
+
         for text in pagify(text, ["\n"]):
             await self.bot.say(escape_mass_mentions(text) + auth)
 
@@ -103,7 +103,8 @@ class say:
         try:
             await self.bot.delete_message(ctx.message)
         except:
-            raise Exception("I do not have the delete messages permissions")
+            await self.bot.say("I do not have the `Manage Messages` permissions")
+            return
 
         colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
         colour = int(colour, 16)
@@ -130,7 +131,8 @@ class say:
         try:
             await self.bot.delete_message(ctx.message)
         except:
-            raise Exception("I do not have the delete messages permissions")
+            await self.bot.say("I do not have the `Manage Messages` permissions")
+            return
 
         colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
         colour = int(colour, 16)
@@ -153,6 +155,85 @@ class say:
         except:
             await self.bot.say("I need the `Embed links` permission to send this")
 
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def embedurl(self, ctx, text: str, url: str = None):
+        """Embed links into a embed"""
+
+        colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        colour = int(colour, 16)
+
+        randnum = randint(1, 10)
+        empty = u"\u2063"
+        emptyrand = empty * randnum
+
+        if not url:
+            data = discord.Embed(description=str(text), colour=discord.Colour(value=colour))
+        else:
+            if text.find("]") != -1:
+                textnumber = text.find("]") + 1
+                url = "({})".format(url)
+                text = text[:textnumber] + url  + text[textnumber:]
+
+                data = discord.Embed(description=str(text), colour=discord.Colour(value=colour))
+
+            else:
+                data = discord.Embed(description=str(text), colour=discord.Colour(value=colour))
+
+
+        if ctx.message.author.avatar_url:
+            data.set_author(name=ctx.message.author.name,
+                            url=ctx.message.author.avatar_url, icon_url=ctx.message.author.avatar_url)
+        else:
+            data.set_author(name=ctx.message.author.name)
+
+        try:
+            await self.bot.say(emptyrand, embed=data)
+        except:
+            await self.bot.say("I need the `Embed links` permission to send this")
+
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.admin_or_permissions(administrator=True)
+    async def embedurladmin(self, ctx, text: str, url: str = None):
+        """Embed links into a embed without knowing who wrote it"""
+
+        try:
+            await self.bot.delete_message(ctx.message)
+        except:
+            await self.bot.say("I do not have the `Manage Messages` permissions")
+            return
+
+        colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        colour = int(colour, 16)
+
+        randnum = randint(1, 10)
+        empty = u"\u2063"
+        emptyrand = empty * randnum
+
+        if not url:
+            data = discord.Embed(description=str(text), colour=discord.Colour(value=colour))
+        else:
+            if text.find("]") != -1:
+                textnumber = text.find("]") + 1
+                url = "({})".format(url)
+                text = text[:textnumber] + url  + text[textnumber:]
+
+                data = discord.Embed(description=str(text), colour=discord.Colour(value=colour))
+
+            else:
+                data = discord.Embed(description=str(text), colour=discord.Colour(value=colour))
+
+
+        if ctx.message.author.avatar_url:
+            data.set_author(name=ctx.message.author.name,
+                            url=ctx.message.author.avatar_url, icon_url=ctx.message.author.avatar_url)
+        else:
+            data.set_author(name=ctx.message.author.name)
+
+        try:
+            await self.bot.say(emptyrand, embed=data)
+        except:
+            await self.bot.say("I need the `Embed links` permission to send this")
 
 def setup(bot):
     bot.add_cog(say(bot))
