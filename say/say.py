9bec56bbc0dc6ccea5b114448bc3735fc9ba9fa1
Sentry <sentrycraft123@gmail.com>
2016-12-04 15:09:24 +0100

    [say] added embedimage and all its forms

--- say/say.py
+++ say/say.py
@@ -15,7 +15,7 @@ class say:
             raise Exception("This Cog does not work with the Admin cog from Squid-Plugins")
 
     @commands.command(pass_context=True, no_pm=True)
-    async def say(self, ctx, *, text):
+    async def say(self, ctx, *, text: str):
         """Says Something as the bot"""
 
         auth = " (message by {})".format(ctx.message.author.mention)
@@ -26,7 +26,7 @@ class say:
 
     @commands.command(pass_context=True, no_pm=True, aliases=["sayop"])
     @checks.admin_or_permissions(administrator=True)
-    async def sayadmin(self, ctx, *, text):
+    async def sayadmin(self, ctx, *, text: str):
         """Says Something as the bot without any trace of the message author"""
         try:
             await self.bot.delete_message(ctx.message)
@@ -40,7 +40,7 @@ class say:
     @commands.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(administrator=True)
     async def sayclear(self, ctx, *, text):
-        """sayadmin command for selfbots"""
+        """sayadmin for selfbots"""
 
         for text in pagify(text, ["\n"]):
             await self.bot.say(escape_mass_mentions(text))
@@ -71,7 +71,7 @@ class say:
 
     @commands.command(pass_context=True, no_pm=True, aliases=["embedsayop"])
     @checks.admin_or_permissions(administrator=True)
-    async def embedsayadmin(self, ctx, *, text):
+    async def embedsayadmin(self, ctx, *, text: str):
         """Says Something as the bot without any trace of the message author in a embed"""
 
         try:
@@ -87,9 +87,7 @@ class say:
         empty = u"\u2063"
         emptyrand = empty * randnum
 
-        data = discord.Embed(
-            description="", colour=discord.Colour(value=colour))
-        data.add_field(name=str(text), value=u"\u2063")
+        data = discord.Embed(description=str(text), colour=discord.Colour(value=colour))
 
         try:
             await self.bot.say(emptyrand, embed=data)
@@ -99,7 +97,7 @@ class say:
 
     @commands.command(pass_context=True, no_pm=True)
     @checks.is_owner()
-    async def embedsayclear(self, ctx, *, text):
+    async def embedsayclear(self, ctx, *, text: str):
         """embedsayadmin for selfbots"""
 
         colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
@@ -137,8 +135,7 @@ class say:
         empty = u"\u2063"
         emptyrand = empty * randnum
 
-        data = discord.Embed(description=str(
-            text), colour=discord.Colour(value=color))
+        data = discord.Embed(description=str(text), colour=discord.Colour(value=colour))
 
         if ctx.message.author.avatar_url:
             data.set_author(name=ctx.message.author.name,
@@ -312,6 +309,91 @@ class say:
             await self.bot.say(emptyrand, embed=data)
         except:
             await self.bot.say("I need the `Embed links` permission to send this")
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def embedimage(self, ctx, *, image: str):
+        """Embed a image as the bot"""
+
+        colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        colour = int(colour, 16)
+
+        randnum = randint(1, 10)
+        empty = u"\u2063"
+        emptyrand = empty * randnum
+
+        if image.endswith(".gif") or image.endswith(".png") or image.endswith(".jpeg") or image.endswith(".jpg"):
+            data = discord.Embed(colour=discord.Colour(value=colour))
+            data.set_image(url=image)
+        else:
+            await self.bot.say("Not a Valid Link")
+            return
+
+        if ctx.message.author.avatar_url:
+            data.set_author(name=ctx.message.author.name,
+                            url=ctx.message.author.avatar_url, icon_url=ctx.message.author.avatar_url)
+        else:
+            data.set_author(name=ctx.message.author.name)
+
+        try:
+            await self.bot.say(emptyrand, embed=data)
+        except:
+            await self.bot.say("I need the `Embed links` permission to send this")
+
+    @commands.command(pass_context=True, no_pm=True, aliases=["embedimageop"])
+    @checks.admin_or_permissions(administrator=True)
+    async def embedimageadmin(self, ctx, *, image: str):
+        """Embed a image as the bot without anyone knowing from who"""
+
+        try:
+            await self.bot.delete_message(ctx.message)
+        except:
+            await self.bot.say("I do not have the `Manage Messages` permissions")
+            return
+
+        colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        colour = int(colour, 16)
+
+        randnum = randint(1, 10)
+        empty = u"\u2063"
+        emptyrand = empty * randnum
+
+        if image.endswith(".gif") or image.endswith(".png") or image.endswith(".jpeg") or image.endswith(".jpg"):
+            data = discord.Embed(colour=discord.Colour(value=colour))
+            data.set_image(url=image)
+        else:
+            await self.bot.say("Not a Valid Link")
+            return
+
+        try:
+            await self.bot.say(emptyrand, embed=data)
+        except:
+            await self.bot.say("I need the `Embed links` permission "
+                               "to send this")
+
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.is_owner()
+    async def embedimageclear(self, ctx, *, image: str):
+        """embedimageadmin for selfbots"""
+
+        colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        colour = int(colour, 16)
+
+        randnum = randint(1, 10)
+        empty = u"\u2063"
+        emptyrand = empty * randnum
+
+        if image.endswith(".gif") or image.endswith(".png") or image.endswith(".jpeg") or image.endswith(".jpg"):
+            data = discord.Embed(colour=discord.Colour(value=colour))
+            data.set_image(url=image)
+        else:
+            await self.bot.say("Not a Valid Link")
+            return
+
+        try:
+            await self.bot.say(emptyrand, embed=data)
+        except:
+            await self.bot.say("I need the `Embed links` permission "
+                               "to send this")
 
 def setup(bot):
     bot.add_cog(say(bot))
