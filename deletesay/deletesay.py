ee0de68941dde07801e780fcd051028978118663
Sentry <sentrycraft123@gmail.com>
2016-12-05 21:37:54 +0100

    [deltesay] forgot __init__

--- deletesay/deletesay.py
+++ deletesay/deletesay.py
@@ -5,6 +5,9 @@ from cogs.utils.chat_formatting import pagify, box
 class deletesay:
     """says a message and deletes it"""
 
+    def __init__(self, bot):
+        self.bot = bot
+        
     @commands.command(pass_context=True, no_pm=True, aliases=["sayop"])
     async def deletesay(self, ctx, *, text: str):
         """Says Something as the bot without any trace of the message author"""
