8cb656a068c2c649e67176e59dc4d5da86e118ba
Sentry <sentrycraft123@gmail.com>
2016-12-05 21:38:15 +0100

    [deltesay] made code look better

--- deletesay/deletesay.py
+++ deletesay/deletesay.py
@@ -2,12 +2,13 @@ import discord
 from discord.ext import commands
 from cogs.utils.chat_formatting import pagify, box
 
+
 class deletesay:
     """says a message and deletes it"""
 
     def __init__(self, bot):
         self.bot = bot
-        
+
     @commands.command(pass_context=True, no_pm=True, aliases=["sayop"])
     async def deletesay(self, ctx, *, text: str):
         """Says Something as the bot without any trace of the message author"""
@@ -19,6 +20,7 @@ class deletesay:
 
         for text in pagify(text, ["\n"]):
             await self.bot.say(escape_mass_mentions(text))
+
 
 def setup(bot):
     bot.add_cog(deletesay(bot))
