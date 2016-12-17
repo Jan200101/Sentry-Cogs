2d84c89ede341aab766018218ac586dfd3066194
Sentry <sentrycraft123@gmail.com>
2016-12-17 17:58:54 +0100

    added math

---
+++ math/math.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+class math:
+    """Math interpreter in Discord"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commmands.command(pass_context=True)
+    async def math(self, ctx, math):
+        """Does math you are too lazy for"""
+
+        math = math.replace(" ", "")
+
+        calculation = eval(math)
+
+        await self.bot.say("{} **=** {}".format(math, calculation))
+
+
+
+def setup(bot):
+    bot.add_cog(math(bot))
