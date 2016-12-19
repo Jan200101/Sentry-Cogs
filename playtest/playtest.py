9b2b5561026518e966f019afb3783b5ab4ed98ef
Sentry <sentrycraft123@gmail.com>
2016-12-19 19:17:38 +0100

    [playtest] removed seconds

--- playtest/playtest.py
+++ playtest/playtest.py
@@ -136,9 +136,6 @@ class playtest:
         if z.minutes != 0:
             k += "{} minutes ".format(z.minutes)
 
-        if z.seconds != 0:
-            k += "{} seconds ".format(z.seconds)
-
 
         if z.seconds != 0: #Bunch of color code stuff for certain times
             color = discord.colour.Color.blue()
@@ -174,8 +171,8 @@ class playtest:
 
         return data
 
-    @commands.command()
-    async def playtest(self):
+    @commands.command(aliases=["nextplaytest","playtest"])
+    async def playtestinfo(self):
         """"""
 
         msg = await self.get_playtest()
@@ -191,7 +188,7 @@ class playtest:
         """Sets how often the playtest information gets updated"""
         if await self._int(seconds):
             if seconds < 10:
-                message = '`I can\'t do that, the refresh rate has to 10 seconds or above to not angry the Google Api`'
+                message = '`I can\'t do that, the refresh rate has to 10 seconds or above to not make the Google API angry`'
             else:
                 self.refresh_rate = seconds
                 self.settings['REFRESH_RATE'] = self.refresh_rate
@@ -208,12 +205,12 @@ class playtest:
         if len(channel) > 0:
             self.settings['CHANNEL_ID'] = str(channel[0].id)
             dataIO.save_json('data/statistics/settings.json', self.settings)
-            message = 'Channel set to {}'.format(channel[0].mention)
+            message = "`Channel set to #{}`.format(channel[0].name)
         elif not self.settings['CHANNEL_ID']:
             message = 'No channel set!'
         else:
             channel = discord.utils.get(self.bot.get_all_channels(), id=self.settings['CHANNEL_ID'])
-            message = 'Current channel is {}'.format(channel.mention)
+            message = '`Current channel is #{}`'.format(channel.name)
         await self.bot.say(message)
 
     async def reload_playtest(self): #Reloads the automated playtest. Thank Paddo
