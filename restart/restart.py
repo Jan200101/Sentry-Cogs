9234f44ff445f884c923247739a0463a81d54a81
Sentry <sentrycraft123@gmail.com>
2017-01-03 15:12:32 +0100

    made restart cog

---
+++ restart/restart.py
@@ -0,0 +1,37 @@
+from discord.ext import commands
+from cogs.utils import checks
+import time
+import sys
+import subprocess
+import os
+
+class restart:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def restart(self, ctx):
+        await self.bot.say("Do you really want to restart? (yes/no)")
+        answer = await self.bot.wait_for_message(timeout=10,
+                                                 author=ctx.message.author)
+
+        if answer == None:
+            await self.bot.say("Canceling restart...")
+        elif answer.content.lower().strip() == "yes":
+
+            await self.bot.say("Restarting now.")
+            time.sleep(2)
+            cd = "{}/{}".format(os.getcwd(), sys.argv[0])
+            child = subprocess.Popen(["{} {}".format(sys.executable, cd)],shell=True,stdout=subprocess.PIPE)
+            output,error = child.communicate()
+            print("Output\n{}\nError\n{}\n".format(output, error))
+            sys.exit()
+
+        else:
+            await self.bot.say("Canceling restart...")
+
+def setup(bot):
+    n = restart(bot)
+    bot.add_cog(n)
