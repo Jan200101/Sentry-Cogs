1d7d1a42e778b5b5ac94113d99e6b1ab8dc7644c
Jan200101 <sentrycraft123@gmail.com>
2017-01-17 16:43:15 +0100

    added lenny cog

---
+++ lenny/lenny.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import re
+import urllib.request
+import sys
+
+class lenny:
+    """┬─┬ノ( ´ᗝ`ノ)"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def lenny(self):
+        """( ͡° ͜ʖ ͡°)"""
+
+        regex = r':"(\S+)"'
+        lenny = urllib.request.urlopen('http://lenny.today/api/v1/random?limit=1').read().decode('utf-8')
+
+        matches = re.findall(regex, lenny)
+
+        for match in matches:
+            try:
+                await self.bot.say(match)
+            except:
+                await self.bot.say("Could Lenny")
+
+
+def setup(bot):
+    bot.add_cog(lenny(bot))
