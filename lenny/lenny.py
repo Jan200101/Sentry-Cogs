657ad442566c71e478aacc615a7efc77d82cb579
Jan200101 <sentrycraft123@gmail.com>
2017-01-20 18:10:14 +0100

    Changed max count Added some help for new coders

--- lenny/lenny.py
+++ lenny/lenny.py
@@ -14,27 +14,31 @@ class lenny:
     async def lenny(self, count:int=1):
         """Lenny Command"""
 
-        if count > 20:
-            await self.bot.say("Cannot post more than 20 lennys")
+        maxcount = 15  # edit this number for a higher lenny count (updating will reset the cog)
+
+        if count > maxcount:
+            await self.bot.say("Cannot post more than {} lennys".format(maxcount))
             return
 
-        gateway = 'http://lenny.today/api/v1/random?limit={}'.format(count)
+        gateway = 'http://lenny.today/api/v1/random?limit={}'.format(count) # uses lenny.today as a api that returns dicts.
         payload = {}
         payload['limit'] = 1
         headers = {'user-agent': 'Red-cog/1.0'}
+
         session = aiohttp.ClientSession()
         async with session.get(gateway, params=payload, headers=headers) as r:
             lenny = await r.json()
-        session.close()
+
+        session.close() # closes the session to close the connection to the site
 
         try:
             lennylist = []
             for x in lenny:
-                lennylist.append("{}\n".format(x['face']))
+                lennylist.append("{}\n".format(x['face'])) # gets the face (lenny) out of dict and puts it in a list
 
-            lenny = "".join(lennylist)
+            lenny = "".join(lennylist) # merges the list into a string
 
-            await self.bot.say(lenny)
+            await self.bot.say(lenny) # says the lennys
             return
         except:
             return
