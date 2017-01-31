c1caca0a4abe9b74f70b76b25ec36f6c08db7ea7
Jan200101 <sentrycraft123@gmail.com>
2017-01-31 16:09:18 +0100

    language fix typo fix

--- updatestatus/updatestatus.py
+++ updatestatus/updatestatus.py
@@ -1,8 +1,9 @@
 import discord
 import os
+from time import sleep
 from discord.ext import commands
-import asyncio
-import requests
+from asyncio import wait_for
+from requests import get
 
 
 class UpdateStatus:
@@ -15,7 +16,7 @@ class UpdateStatus:
     async def behind(self):
         """Shows how many commits you are behind"""
         response = self.bot.loop.run_in_executor(None, self._get_behind)
-        result = await asyncio.wait_for(response, timeout=20)
+        result = await wait_for(response, timeout=20)
 
         try:
             await self.bot.say(embed=result)
@@ -28,7 +29,7 @@ class UpdateStatus:
     async def travis(self):
         """Shows travis status of your reds installation"""
         response = self.bot.loop.run_in_executor(None, self._get_travis)
-        result = await asyncio.wait_for(response, timeout=20)
+        result = await wait_for(response, timeout=20)
 
         try:
             await self.bot.say(embed=result)
@@ -39,12 +40,14 @@ class UpdateStatus:
 
     def _get_behind(self):
 
-        branch = os.popen(r'LC_ALL=C git status git rev-parse --abbrev-ref HEAD').read().strip()
+        os.popen(r'LC_ALL=C git fetch')
 
-        os.popen(r'LC_ALL=C git status git fetch')
+        sleep(1)
+
+        branch = os.popen(r'LC_ALL=C git rev-parse --abbrev-ref HEAD').read().strip()
 
         # checks if local is out of date, needs the fetch first
-        status = os.popen(r'LC_ALL=C git status git status -uno').read().strip()
+        status = os.popen(r'LC_ALL=C git status -uno').read().strip()
 
         if status.find("Your branch is up-to-date") != -1:
             behind = "Your bot is up to date"
@@ -65,13 +68,13 @@ class UpdateStatus:
         return embed
 
     def _get_travis(self):
-        branch = os.popen(r'LC_ALL=C git status git rev-parse --abbrev-ref HEAD')
+        branch = os.popen(r'LC_ALL=Cgit rev-parse --abbrev-ref HEAD')
         branch = branch.read().strip()
 
-        allbranches = os.popen(r'LC_ALL=C git status git branch')
+        allbranches = os.popen(r'LC_ALL=C git branch')
         allbranches = allbranches.read().strip()
 
-        url = os.popen(r'LC_ALL=C git status git config --get remote.origin.url')
+        url = os.popen(r'LC_ALL=C git config --get remote.origin.url')
         url = url.read().strip()
         if url.endswith(".git"):
             url = url[:-4]
@@ -105,7 +108,7 @@ class UpdateStatus:
 
         # makes a quick test if the image is even accessable if not gives out a
         # error
-        request = requests.get(travisbuildstatus)
+        request = get(travisbuildstatus)
         if request.status_code == 200:
             embed.set_image(url=travisbuildstatus)
         else:
@@ -118,7 +121,7 @@ class UpdateStatus:
 def check_folder():
     if not os.path.exists(".git"):
         raise Exception(
-            "\n\n You did not clone red using git. Please return to the guide and follow its instructions")
+            "\n\nYou did not clone red using git. Please return to the guide and follow its instructions\n")
 
 
 def setup(bot):
