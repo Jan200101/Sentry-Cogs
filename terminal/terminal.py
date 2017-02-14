f1ae60f342caa49a140d1c8b0ee3266fbc7c0f29
Jan200101 <sentrycraft123@gmail.com>
2017-02-14 11:56:24 +0100

    [Terminal]  added a pip and apt-get routine check             added pip and apt-get confirmation

--- terminal/terminal.py
+++ terminal/terminal.py
@@ -35,9 +35,9 @@ class Terminal:
 
         await self.bot.say(box(release(), 'Bash'))
 
-    @commands.command(aliases=["cmd", "terminal"])
+    @commands.command(pass_context=True, aliases=["cmd", "terminal"])
     @checks.is_owner()
-    async def shell(self, *, command: str):
+    async def shell(self, ctx, *, command: str):
         """Terminal inside Discord"""
 
         # List of blocked commands
@@ -45,7 +45,6 @@ class Terminal:
         # Make sure commands are in lowercase
         blacklist = []
         whitelist = []
-
 
         if command.find("&") != -1:
             command = command.split("&")[0]
@@ -61,13 +60,31 @@ class Terminal:
                 await self.bot.say("'{}' is on the command blacklist".format(command))
                 return
 
-        if command.lower().find("apt-get") != -1 and command.lower().find("-y") == -1:
+        if command.lower().find("apt-get install") != -1 and command.lower().find("-y") == -1:
             command = "{} -y".format(command)
 
+        if command.lower().find("apt-get install") != -1 or command.lower().find("pip") != -1 and command.lower().find('install') != -1:
+            # Storing text in variables for easier editing
+            msg = 'WARNING\nUsing apt-get or pip with this cog\nmay result in multiple problems\nranging from Red freezing up to bigger stuff\nDo you really want to continue ? (yes/no)'
+            cancel = "Canceling"
+            contin = "Please Wait...\nThis process can take some time based on which package is getting installed"
+
+            await self.bot.say(box(msg, 'Prolog'))
+            answer = await self.bot.wait_for_message(timeout=10, author=ctx.message.author)
+
+            if answer.content.lower().strip() != 'yes':
+                await self.bot.say(box(cancel, 'Prolog'))
+                return
+
+            await self.bot.say(box(contin, 'Prolog'))
+
         try:
-            output = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT).communicate()[0]
+            # main command routine
+            output = Popen(command, shell=True, stdout=PIPE,
+                           stderr=STDOUT).communicate()[0]
             error = False
         except CalledProcessError as e:
+            # error routine in the worst cases
             try:
                 output = e.output
             except:
@@ -75,6 +92,7 @@ class Terminal:
             error = True
 
         # Decode to unicode for full character support
+        # Printint this in the console may result in errors
         shell = output.decode('utf_8')
 
         if shell == "" and not error:
