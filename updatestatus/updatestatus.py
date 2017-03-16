5119ecce7124bec0511e5a9786d8096b9cbddc97
Jan200101 <sentrycraft123@gmail.com>
2017-03-16 15:41:40 +0100

    [UpdateStatus] added update

--- updatestatus/updatestatus.py
+++ updatestatus/updatestatus.py
@@ -39,6 +39,27 @@ class UpdateStatus:
         except:
             await self.bot.say("a error happend")
 
+
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def update(self, ctx):
+        """Shows how many commits you are behind"""
+
+        await self.bot.say('This will get rid of any edits you havent saved. Continue ? (yes/no)')
+        answer = await self.bot.wait_for_message(timeout=10, author=ctx.message.author)
+
+        if answer == None:
+            await self.bot.say('Canceling Update')
+            return
+        elif answer.content.lower().strip() != 'yes':
+            await self.bot.say('Canceling Update')
+            return
+
+        response = self.bot.loop.run_in_executor(None, self._update)
+        result = await wait_for(response, timeout=20)
+
+        await self.bot.say(embed=result)
+
     def _get_behind(self):
 
         if os.name != 'nt':
@@ -123,6 +144,39 @@ class UpdateStatus:
 
         return embed
 
+    def _update(self):
+
+        if os.name != 'nt':
+            os.popen('LC_ALL=C')
+
+        os.popen(r'git reset --hard')
+        os.popen(r'git stash')
+
+        sleep(1)
+
+        os.popen(r'git pull')
+
+        sleep(1)
+
+        # checks if local is out of date, needs the fetch first
+        status = os.popen(r'git status -uno').read().strip()
+
+        if status.find("Your branch is up-to-date") != -1:
+            behind = "Update successfull"
+            color = discord.Colour.green()
+
+        elif status.find("Your branch is behind") != -1:
+            behind = "Update unsuccessfull"
+            color = discord.Colour.red()
+
+        else:
+            behind = "Unable to check if out of date"  # just here in the worst case
+            color = discord.Colour.orange()
+
+        embed = discord.Embed(title=behind,
+                              colour=color)
+
+        return embed
 
 def check_folder():
     if not os.path.exists(".git"):
